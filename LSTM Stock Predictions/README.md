18b4c597fe2fd6407bc530f9b3831482e811a49c
Venkat-Rajgopal <venkatramani.r@gmail.com>
2018-11-07 22:24:21 +0100

    fixed model

--- LSTM Stock Predictions/README.md
+++ LSTM Stock Predictions/README.md
@@ -1,9 +1,11 @@
 # Predicting S&P500 closing prices using a Recurrent neural network. 
 
 ## Data
-The data for this is obtained from Yahoo. These can be downloaded from [here](https://finance.yahoo.com/quote/%5EGSPC/history?p=%5EGSPC). 
+The data for this is obtained from Yahoo. These can be downloaded from [here](https://finance.yahoo.com/quote/%5EGSPC/history?p=%5EGSPC).  A look at the closing prices. 
 
-A look at the closing prices. 
+
 ![](plots/closing_prices.png)
+
+___
 
 
--- LSTM Stock Predictions/helper.py
+++ LSTM Stock Predictions/helper.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.style as style
+import numpy as np
+
 style.use('fivethirtyeight')
 from sklearn.preprocessing import MinMaxScaler
 
@@ -44,4 +46,18 @@ def load_data(inputdata, seq_len, val_percent):
         
     return x_train, y_train, x_valid, y_valid
 
+
+# function to get the next batch data
+def get_next_batch(batch_size):
+    global index_in_epoch, x_train, perm_array   
+    start = index_in_epoch
+    index_in_epoch += batch_size
+    
+    if index_in_epoch > x_train.shape[0]:
+        np.random.shuffle(perm_array) # shuffle permutation array
+        start = 0 # start next epoch
+        index_in_epoch = batch_size
+        
+    end = index_in_epoch
+    return x_train[perm_array[start:end]], y_train[perm_array[start:end]]
 
