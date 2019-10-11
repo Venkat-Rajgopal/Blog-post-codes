9eea9c2c516a22b5726eb1c5e3a6b76357a6f01b
venkat-rajgopal <venkatramani.r@gmail.com>
2019-10-11 23:49:50 +0200

    code cleanup

---
+++ R-ADAM vs ADAM/README.md
@@ -0,0 +1,17 @@
+
+# R-Adam
+**Tensoflow 2** implmentations of of the comparison between R-Adam and Adam.  
+[Cross posted: R-Adam vs ADAM](https://venkat-rajgopal.github.io/Rectified-ADAM-optimizer/) 
+
+`python3` usage: 
+```
+python3 train_cfar.py --optimizer adam --nepochs 100 --plot adam.png 
+python3 train_cfar.py --optimizer radam --nepochs 100 --plot radam.png 
+```
+
+## Results
+<img src="adam.png" alt="adam" width="450"/>
+
+<img src="radam.png" alt="radam" width="450"/>
+
+
--- R-ADAM vs ADAM/train_cfar.py
+++ R-ADAM vs ADAM/train_cfar.py
@@ -2,12 +2,6 @@
 # usage
 # python3 train_cfar_v1.py --optimizer radam --plot radam.png
 
-
-import matplotlib
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
-import numpy as np
-
 import argparse
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import classification_report
@@ -23,54 +17,52 @@ from RAdam import RAdamOptimizer
 from tensorflow.python.keras.utils import to_categorical
 import os 
 os.environ['TF_KERAS'] = '1'
-
 from keras_radam import RAdam
 
-
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import numpy as np
+# -------------------------------------------------------------------------------------------------
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-o", "--optimizer", type=str, default="adam",
-	            choices=["adam", "radam"],
-	            help="type of optmizer")
-ap.add_argument("-c", "--nepochs", type=int, required=True,
-	    help="no of epochs")				
-ap.add_argument("-p", "--plot", type=str, required=True,
-	    help="path to output training plot")
-
+ap.add_argument("-o", "--optimizer", type=str, default="adam", choices=["adam", "radam"], help="type of optmizer")
+ap.add_argument("-c", "--nepochs", type=int, required=True, help="no of epochs")				
+ap.add_argument("-p", "--plot", type=str, required=True, help="path to output training plot")
 args = vars(ap.parse_args())
 
 
+# Choice of optimizers
 if args["optimizer"] == "adam":
-	# initialize the Adam optimizer
+
 	print("Using Adam")
 	opt = Adam(lr=1e-3)
 
-# otherwise, we are using Rectified Adam
 else:
     print("Using Rectified-Adam")
     opt = RAdam(total_steps=5000, warmup_proportion=0.1, min_lr=1e-5)
-	#opt = RAdamOptimizer(learning_rate=0.0001, beta1=0.9, beta2=0.999, weight_decay=0.0)
-   # opt = RAdam(total_steps=5000, warmup_proportion=0.1, min_lr=1e-5)
 
 
-# load train and test dataset
+# load train and test dataset and convert to cat
 def load_dataset():
-	# load dataset
 	(trainX, trainY), (testX, testY) = cifar10.load_data()
+
 	# one hot encode target values
 	trainY = to_categorical(trainY)
 	testY = to_categorical(testY)
+
 	return trainX, trainY, testX, testY
  
 # scale pixels
 def scale_pixels(train, test):
-	# convert from integers to floats
+	# convert integers to floats
 	train_norm = train.astype('float32')
 	test_norm = test.astype('float32')
+
 	# normalize to range 0-1
 	train_norm = train_norm / 255.0
 	test_norm = test_norm / 255.0
-	# return normalized images
+
 	return train_norm, test_norm
 
 
@@ -97,26 +89,7 @@ def define_model(opt = opt):
 	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 	return model
 
-
-
-def plot_results(history, plotname):
-
-    plt.subplot(211)
-    plt.title('Cross Enthropy loss')
-    plt.plot(history.history['loss'], color = 'blue', label='train')
-    plt.plot(history.history['val_loss'], color = 'orange', label='test')
-
-    # plt accuracy
-    plt.subplot(212)
-    plt.title('Classification Accuracy')
-    plt.plot(history.history['accuracy'], color = 'blue', label='test')
-    plt.plot(history.history['val_accuracy'], color = 'orange', label='test')
-
-    plt.savefig(plotname)
-    plt.close()
-
-
-def plot_results_(history, plotname, n_epochs, subtitle):
+def plot_results(history, plotname, n_epochs, subtitle):
 
 	n = np.arange(0, n_epochs)
 	plt.style.use('seaborn')
@@ -134,25 +107,23 @@ def plot_results_(history, plotname, n_epochs, subtitle):
 	plt.close()
 
 
-
-
-# run the test harness for evaluating a model
+# run model
 def evaluate_model(opt = opt, plotname = args['plot'], n_epochs = args['nepochs']):
-	# load dataset
+	# load dataset and scale
 	trainX, trainY, testX, testY = load_dataset()
-	# prepare pixel data
 	trainX, testX = scale_pixels(trainX, testX)
-	# define model
+
+	# define model and fit 2d model
 	model = define_model(opt = opt)
-	# fit model
 	history = model.fit(trainX, trainY, epochs=n_epochs, batch_size=64, validation_data=(testX, testY), verbose=1)
 	print(history.history)
 	
 	# evaluate model
 	_, acc = model.evaluate(testX, testY, verbose=1)
 	print('> %.3f' % (acc * 100.0))
-	# learning curves
-	plot_results_(history, plotname = plotname, n_epochs = n_epochs, subtitle = args['optimizer'])
+
+	# plot learning curves
+	plot_results(history, plotname = plotname, n_epochs = n_epochs, subtitle = args['optimizer'])
  
-# entry point, run the test harness
+# Call model function
 evaluate_model()
